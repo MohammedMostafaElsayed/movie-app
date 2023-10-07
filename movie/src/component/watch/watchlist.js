d00d8829e01a8c78e650df9662f363deb7be4941
mohammed <mm994225@gmail.com>
2023-10-08 01:32:23 +0300

    start app

---
+++ movie/src/component/watch/watchlist.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import WatchCard from "./watchCard";
+
+export default function WatchList(){
+    const items = useSelector(state => state.watch.items);
+
+    return(
+        <>
+            <h3>Watch list</h3>
+            <div class="row row-cols-2 g-4 m-5">
+                {
+                    items.map((w, i)=>{
+                        return(<WatchCard item={w}/>);
+                        
+                    })
+                }
+            </div>
+        
+        </>
+        
+    );
+
+}
---
+++ movie/src/pages/watchPage.js
@@ -0,0 +1,7 @@
+import WatchList from "../component/watch/watchlist";
+
+export default function WatchPage(){
+    return(
+        <WatchList/>
+    );
+}
